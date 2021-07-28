1b44b18ba6c9197f792992a0baee47583e6ac47f
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-28 10:54:35 -0300

    feat: :lipstick: create logo component
    create text logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Text = styled.h1`
+font-family: 'Shadows Into Light', cursive;
+font-size: 79px;
+color:white;
+letter-spacing: -3px;
+`
+export function Logo() {
+    return <Text>My Kanban</Text>
+}
