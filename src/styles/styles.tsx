c43998712f2ad9f3fd3f2a4b09d6c1d177fb5f91
Caio Vieira <caiocesarav0208@gmail.com>
2021-07-28 10:51:01 -0300

    feat: :lipstick: create global styles

---
+++ src/styles/styles.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html,body{
+
+    width: 100%;
+    height: 100%;
+    background-color:gray;
+    padding: 0 2%;
+}
+`
