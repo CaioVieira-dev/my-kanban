8f151d7ca6d4a7c308e08874c605e2f29f4a5c42
Caio Vieira <caiocesarav0208@gmail.com>
2021-08-02 16:12:23 -0300

    feat: :sparkles: implement firebase
    implement auth context and firebase configs to allow use of firestore

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import { Kanban } from './pages/Kanban'
 import { GlobalStyle } from './styles/styles'
 import { NotesContextProvider } from './contexts/NotesContext'
 import { Login } from './pages/Login'
+import { AuthContextProvider } from './contexts/AuthContextProvider'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact >
-            <NotesContextProvider>
-              <Kanban />
-            </NotesContextProvider>
-          </Route>
-          <Route path="/login" exact component={Login} />
+          <AuthContextProvider>
+
+            <Route path="/" exact >
+              <NotesContextProvider>
+                <Kanban />
+              </NotesContextProvider>
+            </Route>
+            <Route path="/login" exact component={Login} />
+          </AuthContextProvider>
         </Switch>
       </BrowserRouter>
       <GlobalStyle />
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,23 @@
 import { Button, Container, GoogleG } from './styles'
 import { Logo } from "../../components/Logo"
+import { useAuth } from '../../hooks/useAuth'
 
 import G from '../../assets/google.svg'
+import { useHistory } from 'react-router-dom'
 
 export function Login() {
-
+    const { signInWithGoogle, user } = useAuth()
+    const history = useHistory();
+    async function handleLogin() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+        history.push('/');
+    }
     return (
         <Container>
             <Logo className="login" />
-            <Button><GoogleG src={G} />Login com Google</Button>
+            <Button onClick={handleLogin}><GoogleG src={G} />Login com Google</Button>
         </Container>
     )
 }
